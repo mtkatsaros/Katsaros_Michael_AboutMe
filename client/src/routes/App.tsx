bd4263a0dbe4bee2b361206b064ef3f26412183d
mtkatsaros <60672119+mtkatsaros@users.noreply.github.com>
2023-08-18 23:01:23 -0700

    hooked up project post request to the UI

---
+++ client/src/routes/App.tsx
@@ -0,0 +1,14 @@
+//import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Link to={`projects/create`}>Create Project</Link>
+    </>
+  )
+}
+
+export default App
--- server/src/index.ts
+++ server/src/index.ts
@@ -6,15 +6,18 @@ import express, {Request, Response} from "express"
 import mongoose from "mongoose"
 const app = express()
 const port = 50000
+import cors from 'cors'
 
-
+app.use(cors({
+    origin: "*",
+}))
 app.use(express.json())
 
 app.get('/', (req:Request, res:Response)=>{
     res.send('Hello there!')
 })
 
-app.post('/projects', createProject)
+app.post('/projects/create', createProject)
 
 mongoose.connect(process.env.MONGO_URL!).then(()=>{
     console.log(`listening on port ${port}`)
